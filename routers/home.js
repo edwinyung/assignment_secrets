a224d58adf16278c492aab1df845241f99d77aa0
EdwinYung <eyung7@gmail.com>
2017-12-11 14:00:12 -0500

    got req.body.secret

--- routers/home.js
+++ routers/home.js
@@ -1,53 +1,58 @@
-"use strict";
+'use strict';
 
 //==================
 // home router
 //==================
 
-const Express = require("express");
+const Express = require('express');
 const router = Express.Router();
-const User = require("../models/User");
+const User = require('../models/User');
 const {
   createSignedSessionId,
   loginMiddleware,
   loggedInOnly,
   loggedOutOnly
-} = require("../services/Session");
+} = require('../services/Session');
 
 // Home route
 // 1
-router.get("/", loggedInOnly, (req, res) => {
-  res.render("welcome/index");
+router.get('/', loggedInOnly, (req, res) => {
+  res.render('welcome/index');
 });
 
 // Login routes
 // 2
-router.get("/login", loggedOutOnly, (req, res) => {
-  res.render("login");
+router.get('/login', loggedOutOnly, (req, res) => {
+  res.render('login');
 });
 
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
   // 3
   const { username, password } = req.body;
 
   User.findOne({ username }, (err, user) => {
-    if (!user) return res.send("NO USER");
+    if (!user) return res.send('NO USER');
 
     // 4
     if (user.validatePassword(password)) {
       const sessionId = createSignedSessionId(username);
-      res.cookie("sessionId", sessionId);
-      res.redirect("/");
+      res.cookie('sessionId', sessionId);
+      res.redirect('/');
     } else {
-      res.send("UNCOOL");
+      res.send('UNCOOL');
     }
   });
 });
 
 // Logout route
-router.get("/logout", (req, res) => {
-  res.cookie("sessionId", "", { expires: new Date() });
-  res.redirect("/");
+router.get('/logout', (req, res) => {
+  res.cookie('sessionId', '', { expires: new Date() });
+  res.redirect('/');
+});
+
+//Post secret route
+router.post('/home', (req, res) => {
+  console.log(req.body.secret);
 });
 
 module.exports = router;
