1ebe729f383ca0038750542dde4156c232a48931
EdwinYung <eyung7@gmail.com>
2017-12-11 17:57:28 -0500

    req.user

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const home = require("./routers/home");
-const register = require("./routers/register");
+const home = require('./routers/home');
+const register = require('./routers/register');
 
 // ----------------------------------------
 // App Variables
 // ----------------------------------------
-app.locals.appName = "Secrets";
+app.locals.appName = 'Secrets';
 
 // ----------------------------------------
 // ENV
 // ----------------------------------------
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
 // ----------------------------------------
 // Body Parser
 // ----------------------------------------
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Connect to our mongo server
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/test");
-
+// ----------------------------------------
+//middleware to connect to MongoDB via mongoose in your `app.js`
+// ----------------------------------------
+var mongoose = require('mongoose');
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState) {
+    next();
+  } else {
+    require('./mongo')().then(() => next());
+  }
+});
 // ----------------------------------------
 // Sessions/Cookies
 // ----------------------------------------
-const cookieParser = require("cookie-parser");
-const cookieSession = require("cookie-session");
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 
 app.use(cookieParser());
 app.use(
   cookieSession({
-    name: "session",
-    keys: [process.env.SESSION_SECRET || "secret"]
+    name: 'session',
+    keys: [process.env.SESSION_SECRET || 'secret']
   })
 );
 
@@ -47,14 +54,14 @@ app.use((req, res, next) => {
 // ----------------------------------------
 // Flash Messages
 // ----------------------------------------
-const flash = require("express-flash-messages");
+const flash = require('express-flash-messages');
 app.use(flash());
 
 // ----------------------------------------
 // Method Override
 // ----------------------------------------
-const methodOverride = require("method-override");
-const getPostSupport = require("express-method-override-get-post-support");
+const methodOverride = require('method-override');
+const getPostSupport = require('express-method-override-get-post-support');
 
 app.use(
   methodOverride(
@@ -67,7 +74,7 @@ app.use(
 // Referrer
 // ----------------------------------------
 app.use((req, res, next) => {
-  req.session.backUrl = req.header("Referer") || "/";
+  req.session.backUrl = req.header('Referer') || '/';
   next();
 });
 
@@ -79,9 +86,9 @@ app.use(express.static(`${__dirname}/public`));
 // ----------------------------------------
 // Logging
 // ----------------------------------------
-const morgan = require("morgan");
-const morganToolkit = require("morgan-toolkit")(morgan, {
-  req: ["cookies" /*, 'signedCookies' */]
+const morgan = require('morgan');
+const morganToolkit = require('morgan-toolkit')(morgan, {
+  req: ['cookies' /*, 'signedCookies' */]
 });
 
 app.use(morganToolkit());
@@ -90,43 +97,43 @@ app.use(morganToolkit());
 // Routes
 // ----------------------------------------
 // Require our User model and Session helpers
-const User = require("./models/User");
+const User = require('./models/User');
 const {
   createSignedSessionId,
   loginMiddleware,
   loggedInOnly,
   loggedOutOnly
-} = require("./services/Session");
+} = require('./services/Session');
 
 // Mount our custom loginMiddleware
 app.use(loginMiddleware);
 
-app.use("/", home);
-app.use("/register", register);
+app.use('/', home);
+app.use('/register', register);
 
 // ----------------------------------------
 // Template Engine
 // ----------------------------------------
-const expressHandlebars = require("express-handlebars");
-const helpers = require("./helpers");
+const expressHandlebars = require('express-handlebars');
+const helpers = require('./helpers');
 
 const hbs = expressHandlebars.create({
   helpers: helpers,
-  partialsDir: "views/",
-  defaultLayout: "application"
+  partialsDir: 'views/',
+  defaultLayout: 'application'
 });
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // ----------------------------------------
 // Server
 // ----------------------------------------
 const port = process.env.PORT || process.argv[2] || 3000;
-const host = "localhost";
+const host = 'localhost';
 
 let args;
-process.env.NODE_ENV === "production" ? (args = [port]) : (args = [port, host]);
+process.env.NODE_ENV === 'production' ? (args = [port]) : (args = [port, host]);
 
 args.push(() => {
   console.log(`Listening: http://${host}:${port}\n`);
@@ -147,7 +154,7 @@ app.use((err, req, res, next) => {
   if (err.stack) {
     err = err.stack;
   }
-  res.status(500).render("errors/500", { error: err });
+  res.status(500).render('errors/500', { error: err });
 });
 
 module.exports = app;
--- routers/home.js
+++ routers/home.js
@@ -1,70 +1,81 @@
-"use strict";
+'use strict';
 
 //==================
 // home router
 //==================
 
-const Express = require("express");
+const Express = require('express');
 const router = Express.Router();
-const User = require("./../models/User");
-const Secret = require("./../models/Secret");
-// const models = require("./../models");
-// const mongoose = require("mongoose");
-// const User = mongoose.models("User");
-// const Secret = mongoose.models("Secret");
+const mongoose = require('mongoose');
+const models = require('./../models');
+var Secret = mongoose.model('Secret');
+var User = mongoose.model('User');
+
 const {
   createSignedSessionId,
   loginMiddleware,
   loggedInOnly,
   loggedOutOnly
-} = require("../services/Session");
+} = require('../services/Session');
 
 // Home route
 // 1
-router.get("/", loggedInOnly, (req, res) => {
-  res.render("welcome/index", { userSecrets: User.secrets });
+router.get('/', loggedInOnly, async (req, res) => {
+  try {
+    const user = await User.findById(req.session.user);
+
+    res.render('welcome/index', { userSecrets: user.secrets });
+  } catch (e) {
+    next(e);
+  }
 });
 
 // Login routes
 // 2
-router.get("/login", loggedOutOnly, (req, res) => {
-  res.render("login");
+router.get('/login', loggedOutOnly, (req, res) => {
+  res.render('login');
 });
 
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
   // 3
   const { username, password } = req.body;
 
   User.findOne({ username }, (err, user) => {
-    if (!user) return res.send("NO USER");
+    if (!user) return res.send('NO USER');
 
     // 4
     if (user.validatePassword(password)) {
       const sessionId = createSignedSessionId(username);
-      res.cookie("sessionId", sessionId);
-      res.redirect("/");
+      res.cookie('sessionId', sessionId);
+      req.session.user = user.id;
+      res.redirect('/');
     } else {
-      res.send("UNCOOL");
+      res.send('UNCOOL');
     }
   });
 });
 
 // Logout route
-router.get("/logout", (req, res) => {
-  res.cookie("sessionId", "", { expires: new Date() });
-  res.redirect("/");
+router.get('/logout', (req, res) => {
+  res.cookie('sessionId', '', { expires: new Date() });
+  res.redirect('/');
 });
 
 //Post secret route
-router.post("/home", (req, res) => {
-  const { secret } = req.body;
-  const { authorId } = User._id;
-  const newSecret = new Secret({ secret, authorId });
-  newSecret.save(err => {
-    if (err) return res.send("ERROR");
-    res.redirect("/home");
-  });
-  User.secrets.push(newSecret.secretId);
+router.post('/', async (req, res) => {
+  try {
+    const secret = req.body.secret;
+    const authorId = req.session.user;
+    const newSecret = new Secret({ secret, authorId });
+    await newSecret.save();
+    const user = await User.findById(authorId);
+    if (!user) return res.send('NO USER');
+    user.secrets.push(newSecret.id); //push secret to user's array
+    await user.save();
+    res.redirect('/');
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
