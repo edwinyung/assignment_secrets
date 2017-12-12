049d1f2b998d8ba56c7de31afcfd2b9a48da632f
EdwinYung <eyung7@gmail.com>
2017-12-11 20:12:47 -0500

    initial 2.0

--- routers/home.js
+++ routers/home.js
@@ -1,51 +1,49 @@
-"use strict";
+'use strict';
 
 //==================
 // home router
 //==================
 
-const Express = require("express");
+const Express = require('express');
 const router = Express.Router();
-const mongoose = require("mongoose");
-const models = require("./../models");
-var Secret = mongoose.model("Secret");
-var User = mongoose.model("User");
+const mongoose = require('mongoose');
+const models = require('./../models');
+var Secret = mongoose.model('Secret');
+var User = mongoose.model('User');
 
 const {
   createSignedSessionId,
   loginMiddleware,
   loggedInOnly,
   loggedOutOnly
-} = require("../services/Session");
+} = require('../services/Session');
 
 // Home route
 // 1
-router.get("/", loggedInOnly, async (req, res) => {
+router.get('/', loggedInOnly, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).populate("secrets");
-    //const secretArray = [];
-    // for (let i = 0; i < user.secrets.length; i++) {
-    //   console.log("======");
-    //   console.log(user.id);
-    //   console.log(user.secrets);
-    //   if (user.id === user.secrets[i].authorId) {
-    //     secretArray.push({
-    //       secret: user.secrets[i].secret,
-    //       username: user.username
-    //     });
-    //   }
-    // }
+    const user = await User.findById(req.user.id).populate('secrets');
     const secretArray = [];
-    const userArray = [];
-    for (let i = 0; i < req.user.secrets.length; i++) {
-      let secret = await Secret.findById(req.user.secrets[i]);
-      let secretContent = secret.secret;
-      let secretUser = secret.authorId;
-      secretArray.push(secretContent);
-      userArray.push(secretUser);
+    for (let i = 0; i < user.secrets.length; i++) {
+      if (user.id === user.secrets[i].authorId) {
+        secretArray.push({
+          secret: user.secrets[i].secret,
+          username: user.username
+        });
+      }
     }
-    let currentUsername = req.user.username;
-    res.render("welcome/index", {
+    console.log(user);
+    // const secretArray = [];
+    // const userArray = [];
+    // for (let i = 0; i < req.user.secrets.length; i++) {
+    //   let secret = await Secret.findById(req.user.secrets[i]);
+    //   let secretContent = secret.secret;
+    //   let secretUser = secret.authorId;
+    //   secretArray.push(secretContent);
+    //   userArray.push(secretUser);
+    // }
+    // let currentUsername = req.user.username;
+    res.render('welcome/index', {
       userSecrets: secretArray,
       username1: currentUsername
     });
@@ -56,46 +54,46 @@ router.get("/", loggedInOnly, async (req, res) => {
 
 // Login routes
 // 2
-router.get("/login", loggedOutOnly, (req, res) => {
-  res.render("login");
+router.get('/login', loggedOutOnly, (req, res) => {
+  res.render('login');
 });
 
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
   // 3
   const { username, password } = req.body;
 
   User.findOne({ username }, (err, user) => {
-    if (!user) return res.send("NO USER");
+    if (!user) return res.send('NO USER');
 
     // 4
     if (user.validatePassword(password)) {
       const sessionId = createSignedSessionId(username);
-      res.cookie("sessionId", sessionId);
-      res.redirect("/");
+      res.cookie('sessionId', sessionId);
+      res.redirect('/');
     } else {
-      res.send("UNCOOL");
+      res.send('UNCOOL');
     }
   });
 });
 
 // Logout route
-router.get("/logout", (req, res) => {
-  res.cookie("sessionId", "", { expires: new Date() });
-  res.redirect("/");
+router.get('/logout', (req, res) => {
+  res.cookie('sessionId', '', { expires: new Date() });
+  res.redirect('/');
 });
 
 //Post secret route
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const secret = req.body.secret;
     const authorId = req.user.id;
     const newSecret = new Secret({ secret, authorId });
     await newSecret.save();
     const user = await User.findById(authorId);
-    if (!user) return res.send("NO USER");
+    if (!user) return res.send('NO USER');
     user.secrets.push(newSecret.id); //push secret to user's array
     await user.save();
-    res.redirect("/");
+    res.redirect('/');
   } catch (e) {
     next(e);
   }
